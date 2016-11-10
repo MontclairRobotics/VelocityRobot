817a6b87e158cc31ccbced1fd4ed95f65724c90d
jackhymowitz <jack.hymowitz@gmail.com>
2016-11-09 20:37:56 -0500

    State Machine started and DriveTrain started

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by Hymowitz on 11/9/2016.
+ */
+public class DriveTrain {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StateMachine/State.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.StateMachine;
+
+/**
+ * Created by Hymowitz on 11/9/2016.
+ */
+public interface State {
+}
