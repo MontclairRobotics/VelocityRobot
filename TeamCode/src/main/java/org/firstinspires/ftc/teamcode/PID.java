87e6f39d58802b18bcd8ce2d7fdc044000762f55
jackhymowitz <jack.hymowitz@gmail.com>
2016-11-29 19:46:19 -0500

    PID! PID! PID!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by MHS Robotics on 11/29/2016.
+ *
+ * Uses WPI math still
+ */
+
+public class PID {
+    private double p,i,d;
+    private double minOut=-1,maxOut=1;
+    private double target,totalError,error,lastError,out;
+
+    public PID()
+    {
+    }
+    public PID(double p,double i,double d)
+    {
+        setPID(p,i,d);
+    }
+
+    public PID setPID(double p,double i,double d)
+    {
+        this.p=p;
+        this.i=i;
+        this.d=d;
+        return this;
+    }
+
+    public void setTarget(double t)
+    {
+        target=t;
+    }
+
+    public double update(double val,double sec)
+    {
+        error = target - val;
+        totalError += error*sec;
+        if (i != 0) {
+            double iGain = totalError * i;
+            if (iGain < minOut) {
+                totalError = minOut / i;
+            } else if (iGain > maxOut) {
+                totalError = maxOut / i;
+            }
+        }
+        out = p * error + i * totalError + d * (error - lastError)/sec;
+        lastError = error;
+        return out;
+    }
+    public double getOut()
+    {
+        return out;
+    }
+}
