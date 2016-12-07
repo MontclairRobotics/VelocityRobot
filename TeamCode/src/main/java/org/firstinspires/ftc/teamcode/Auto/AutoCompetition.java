18f4bc5317a3b05fc11f0b9a95c725c3dc4d976a
jackhymowitz <jack.hymowitz@gmail.com>
2016-12-06 20:52:55 -0500

    Finished for now I think

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoCompetition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoCompetition.java
@@ -6,10 +6,10 @@ import org.firstinspires.ftc.teamcode.Auto.States.StateObj;
 import org.firstinspires.ftc.teamcode.Robot;
 
 /**
- * Created by MHS Robotics on 12/6/2016.
+ * Created by MHS Robotics
  */
 
-public class AutoCompetition extends Robot {
+public abstract class AutoCompetition extends Robot {
 
 
     public static final int
@@ -47,6 +47,78 @@ public class AutoCompetition extends Robot {
     {
         machine.update();
     }
+    /*
+    ================================================================
+
+                           AUTO SEQUENCES
+
+    ================================================================
+     */
+    public static class TurnShoot1 extends StateMachine
+    {
+        TurnShoot1(double drive0,double turn1,double drive2,double drive3) {
+            super(
+                    new DriveTurnDrive(drive0,turn1,drive2),
+                    new Shoot(),
+                    new Drive(drive3));
+        }
+    }
+    public static class TurnShoot2 extends StateMachine
+    {
+        TurnShoot2(double drive0,double turn1,double drive2,double drive3) {
+            super(
+                    new LoadBallInIntake(),
+                    new DriveTurnDrive(drive0,turn1,drive2),
+                    new Shoot(),
+                    new LoadBallInShooter(),
+                    new Shoot(),
+                    new Drive(drive3));
+        }
+    }
+
+
+    /*
+    ================================================================
+
+                         COMPLEX AUTO STATES
+
+    ================================================================
+     */
+    public static class DriveTurnDrive extends StateMachine
+    {
+        DriveTurnDrive(double drive0,double turn1,double drive2) {
+            super(
+                    new Drive(drive0),
+                    new Turn(turn1),
+                    new Drive(drive2));
+        }
+    }
+    public static class LoadBallInIntake extends StateMachine
+    {
+        LoadBallInIntake()
+        {
+            super(
+                    new IntakeDownSlow(),
+                    new IntakeThird());
+        }
+    }
+    public static class LoadBallInShooter extends StateMachine
+    {
+        LoadBallInShooter()
+        {
+            super(
+                    new IntakeUp(),
+                    new IntakeHalf()
+            );
+        }
+    }
+    public static class Shoot extends StateMachine
+    {
+        public Shoot()
+        {
+            super(new Delay(4),new ShootUp(),new ShootDown());
+        }
+    }
 
     /*
     ================================================================
@@ -56,22 +128,7 @@ public class AutoCompetition extends Robot {
     ================================================================
      */
 
-    public abstract class AutoState extends StateObj {
-        private double secStart;
-        public void start()
-        {
-            hardware.resetMotorOffset();
-            hardware.setPower(0);
-            hardware.shooter.setPower(0);
-            secStart=secTotal;
-        }
-        public double secInState()
-        {
-            return secTotal-secStart;
-        }
-    }
-
-    public class Drive extends AutoState
+    public static class Drive extends AutoState
     {
         private double dist,diff;
         public Drive(double d)
@@ -85,7 +142,7 @@ public class AutoCompetition extends Robot {
         public boolean isDone(){return diff<TOLERANCE;}
     }
 
-    public class Turn extends AutoState
+    public static class Turn extends AutoState
     {
         private double dist,diff;
         public Turn(double d) { dist=d; }
@@ -95,42 +152,42 @@ public class AutoCompetition extends Robot {
         public boolean isDone(){return diff<TOLERANCE;}
     }
 
-    public class ShootUp extends AutoState
+    public static class ShootUp extends AutoState
     {
         public void update(){shootUp();}
         public boolean isDone(){return shooterIsAtTgt();}
     }
-    public class ShootDown extends AutoState
+    public static class ShootDown extends AutoState
     {
         public void update(){shootDown();}
         public boolean isDone(){return shooterIsAtTgt();}
     }
-    public class IntakeDown extends AutoState
+    public static class IntakeDown extends AutoState
     {
         public void update(){intakeDown();}
         public boolean isDone(){return secInState()>3;}
     }
-    public class IntakeDownSlow extends AutoState
+    public static class IntakeDownSlow extends AutoState
     {
         public void update(){intakeDownSlow();}
         public boolean isDone(){return secInState()>3;}
     }
-    public class IntakeHalf extends AutoState
+    public static class IntakeHalf extends AutoState
     {
         public void update(){intakeHalf();}
         public boolean isDone(){return secInState()>3;}
     }
-    public class IntakeThird extends AutoState
+    public static class IntakeThird extends AutoState
     {
         public void update(){intakeThird();}
         public boolean isDone(){return secInState()>3;}
     }
-    public class IntakeUp extends AutoState
+    public static class IntakeUp extends AutoState
     {
         public void update(){intakeUp();}
         public boolean isDone(){return secInState()>3;}
     }
-    public class Delay extends AutoState
+    public static class Delay extends AutoState
     {
         private double tgt;
         public Delay(double t)
@@ -138,5 +195,20 @@ public class AutoCompetition extends Robot {
             tgt=t;
         }
         public boolean isDone(){return secInState()>tgt;}
+    }
+
+    public static abstract class AutoState extends StateObj {
+        private double secStart;
+        public void start()
+        {
+            hardware.resetMotorOffset();
+            hardware.setPower(0);
+            hardware.shooter.setPower(0);
+            secStart=getSecTotal();
+        }
+        public double secInState()
+        {
+            return getSecTotal()-secStart;
+        }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -31,28 +31,28 @@ public abstract class Robot extends OpMode {
             INTAKE_THIRD_POS = 600,
             INTAKE_HALF_POS = 400,//325,
             INTAKE_UP_POS = -100,
-            //shooter configs
-            SHOOTER_DOWN_POS = 200,//200,
+    //shooter configs
+    SHOOTER_DOWN_POS = 200,//200,
             SHOOTER_UP_POS = -950,
-            //Tolerances
-            SHOOTER_AWAY_TOLERANCE = 50,
+    //Tolerances
+    SHOOTER_AWAY_TOLERANCE = 50,
             INTAKE_AWAY_TOLERANCE = 100;
     //Auto Configs
-    public static final double DEGREES_PER_INCH = 10000/85;
-    public static final double TOLERANCE = 0.5*DEGREES_PER_INCH;
-    public static final double MAX_ENCODER_ACCEL=12*DEGREES_PER_INCH;//TODO TODO TODO
+    public static final double DEGREES_PER_INCH = 10000 / 85;
+    public static final double TOLERANCE = 0.5 * DEGREES_PER_INCH;
+    public static final double MAX_ENCODER_ACCEL = 12 * DEGREES_PER_INCH;//TODO TODO TODO
     //========================================
 
 
-    public double
+    public static double
             //intake variables
-            intakePos=0,
-            //shooter variables
-            shooterPos=0;
+            intakePos = 0,
+    //shooter variables
+    shooterPos = 0;
 
     public static String dp = "%.2f", ip = "%d";
 
-    public double secTotal = 0, secInLoop = 0;
+    private static double secTotal = 0, secInLoop = 0;
 
     @Override
     public final void init() {
@@ -73,20 +73,22 @@ public abstract class Robot extends OpMode {
         time.reset();
         user_start();
     }
-    public void user_start(){}
+
+    public void user_start() {
+    }
 
     @Override
     public final void loop() {
         secInLoop = time.seconds() - secTotal;
         secTotal = time.seconds();
         update();
-        hardware.intake.setTargetPosition((int)(intakePos+hardware.intakeOffset+0.5));
-        if(Math.abs((hardware.shooter.getCurrentPosition()+(int)hardware.shooterOffset)-
-                (shooterPos+(int)hardware.shooterOffset)) > SHOOTER_AWAY_TOLERANCE) {
+        hardware.intake.setTargetPosition((int) (intakePos + hardware.intakeOffset + 0.5));
+        if (Math.abs((hardware.shooter.getCurrentPosition() + (int) hardware.shooterOffset) -
+                (shooterPos + (int) hardware.shooterOffset)) > SHOOTER_AWAY_TOLERANCE) {
             hardware.shooter.setPower(1.0);
-            hardware.shooter.setTargetPosition((int)(shooterPos+hardware.shooterOffset+0.5));
-            telemetry.addData("shooter", "from target: " + Math.abs((hardware.shooter.getCurrentPosition()+(int)hardware.shooterOffset)-
-                    (shooterPos+(int)hardware.shooterOffset)));
+            hardware.shooter.setTargetPosition((int) (shooterPos + hardware.shooterOffset + 0.5));
+            telemetry.addData("shooter", "from target: " + Math.abs((hardware.shooter.getCurrentPosition() + (int) hardware.shooterOffset) -
+                    (shooterPos + (int) hardware.shooterOffset)));
         } else {
             telemetry.addData("shooter", "zero power");
             hardware.shooter.setPower(0);
@@ -103,53 +105,62 @@ public abstract class Robot extends OpMode {
         updateTelemetry(telemetry);
     }
 
-    public double constrainChange(double val,double lastVal,double chg)
+    public static double getSecTotal()
+    {
+        return secTotal;
+    }
+    public static double getSecInLoop()
+    {
+        return secInLoop;
+    }
+
+    public static double constrainChange(double val,double lastVal,double chg)
     {
         return constrain(val,lastVal-chg,lastVal+chg);
     }
 
-    public double constrain(double val,double min,double max)
+    public static double constrain(double val,double min,double max)
     {
         if(val<min)return min;
         if(val>max)return max;
         return val;
     }
 
-    public void shootUp()
+    public static void shootUp()
     {
         setShoot(TeleopCompetition.SHOOTER_UP_POS);
     }
-    public void shootDown()
+    public static void shootDown()
     {
         setShoot(TeleopCompetition.SHOOTER_DOWN_POS);
     }
-    public void intakeDown()
+    public static void intakeDown()
     {
         hardware.intake.setPower(1);
         setIntake(INTAKE_DOWN_POS);
     }
-    public void intakeDownSlow()
+    public static void intakeDownSlow()
     {
         hardware.intake.setPower(0.35);
         setIntake(INTAKE_DOWN_POS);
     }
-    public void intakeHalf()
+    public static void intakeHalf()
     {
         hardware.intake.setPower(0.25);
         setIntake(INTAKE_HALF_POS);
     }
-    public void intakeThird()
+    public static void intakeThird()
     {
         hardware.intake.setPower(0.25);
         setIntake(INTAKE_THIRD_POS);
     }
-    public void intakeUp()
+    public static void intakeUp()
     {
         hardware.intake.setPower(0.3);
         setIntake(INTAKE_UP_POS);
     }
 
-    public void setShoot(double tgt)
+    public static void setShoot(double tgt)
     {
         if(hardware.intake.getCurrentPosition()>INTAKE_HALF_POS-INTAKE_AWAY_TOLERANCE)//todo: flip
         {
@@ -161,28 +172,28 @@ public abstract class Robot extends OpMode {
             intakePos=INTAKE_HALF_POS;
         }
     }
-    public void setIntake(double tgt)
+    public static void setIntake(double tgt)
     {
         intakePos=tgt;
     }
 
-    public boolean intakeIsAtTgt()
+    public static boolean intakeIsAtTgt()
     {
         return intakeIsAt(intakePos);
     }
-    public boolean intakeIsAt(double pos) {
+    public static boolean intakeIsAt(double pos) {
         return isCloseTo(pos, hardware.intake.getCurrentPosition(), INTAKE_AWAY_TOLERANCE);
     }
 
-    public boolean shooterIsAtTgt()
+    public static boolean shooterIsAtTgt()
     {
         return shooterIsAt(shooterPos);
     }
-    public boolean shooterIsAt(double pos) {
+    public static boolean shooterIsAt(double pos) {
         return isCloseTo(pos, hardware.shooter.getCurrentPosition(), SHOOTER_AWAY_TOLERANCE);
     }
 
-    public boolean isCloseTo(double d1, double d2, double tolerance) {
+    public static boolean isCloseTo(double d1, double d2, double tolerance) {
         return Math.abs(d1-d2) < tolerance;
     }
 }
