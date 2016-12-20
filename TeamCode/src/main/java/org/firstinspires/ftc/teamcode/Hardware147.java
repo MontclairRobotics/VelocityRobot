51791616d6244f244b42dbf20d2fb7430bf2474a
Rafi Baum <rafi@ukbaums.com>
2016-12-19 19:46:10 -0500

    COMMIT

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware147.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware147.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 /**
  * Created by rafibaum on 19/12/16.
@@ -11,7 +12,7 @@ public class Hardware147 {
     public DcMotor leftMotor = null;
     public DcMotor rightMotor = null;
 
-    Hardware147
+    HardwareMap hwMap =
 
 }
 
