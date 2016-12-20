6c127fb54b5870c5a374be697d1dd1114dc7ad7b
Rafi Baum <rafi@ukbaums.com>
2016-12-19 19:25:25 -0500

    Will starts SEE RICH WE’RE DOING THINGS

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMap.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by rafibaum on 19/12/16.
+ */
+
+public class HardwareMap {
+
+    public DcMotor leftMotor = null;
+    public DcMotor rightMotor = null;
+}
+
